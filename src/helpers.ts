f23118fa55b72cc23201cc85938a97718ffa47db
Vladislav Nikolayev <luxemate@users.noreply.github.com>
2021-08-12 17:03:52 +0300

    Fail the dependency check if pull request is blocked
    If commit status is left pending, then kodiak keeps pull request in a merge queue and doesn't allow other pull requests to merge. Because this pull request depends on other pull requests, it causes a deadlock.

--- src/helpers.ts
+++ src/helpers.ts
@@ -361,7 +361,7 @@ export class IssueManager {
 			description,
 			sha: pull.head.sha,
 			context: this.config.actionName,
-			state: isBlocked ? 'pending' : 'success',
+			state: isBlocked ? 'failure' : 'success',
 		});
 	}
 }
